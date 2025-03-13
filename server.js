7985814cefeea662b09cecb28f3f1807420a8919
Parameswar916 <parameswar0206@gmail.com>
2025-03-14 02:10:47 +0530

    Added test API route

--- server.js
+++ server.js
@@ -56,9 +56,11 @@ app.delete('/movies/:id', async (req, res) => {
     res.json({ message: 'Review deleted' });
   } catch (err) {
     res.status(500).json({ error: err.message });
-  }
+  } 
 });
 
 // Start server
 const PORT = process.env.PORT || 5000;
+app.get("/api/test", (req, res) => {
+       res.json({ message: "Backend is working!"});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
