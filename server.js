46da1b0519ba31309aa5f4d87c1456f2e2a1a0bd
Parameswar916 <parameswar0206@gmail.com>
2025-03-14 02:27:50 +0530

    Fixed template literal issue

--- server.js
+++ server.js
@@ -63,4 +63,5 @@ app.delete('/movies/:id', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.get("/api/test", (req, res) => {
        res.json({ message: "Backend is working!"});
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
